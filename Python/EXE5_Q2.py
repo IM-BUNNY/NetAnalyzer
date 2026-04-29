c92918a4da1d7ea4f15c0012be75e81d6ea836c7
Ayush Thakur <user1bunny@gmail.com>
2026-04-29 16:58:57 +0530

    Delete Python directory

--- Python/EXE5_Q2.py
+++
@@ -1,9 +0,0 @@
-def in_range_or_not(number):
-    return abs(number - 1000) <= 100 or abs(number - 2000) <= 100
-num = int(input("Enter a number: "))
-result = in_range_or_not(num)
-if result:
-    print(f"The number {num} is within 100 of 1000 or 2000.")
-else:
-    print(f"The number {num} is not within 100 of 1000 or 2000.")
-    print("")
