8da055eee75aa11057d8256a8864d53c3f291f74
Ayush Thakur <user1bunny@gmail.com>
2026-04-29 16:58:10 +0530

    Delete C directory

--- C/OS Lab /2.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-/* function main begins program execution */
-int main() {
-printf("%d", rand());
-return 0; /* indicates successful termination */
-} /* end main */
-
